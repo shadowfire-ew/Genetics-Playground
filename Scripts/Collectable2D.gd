5c28769f498c1f191e7ca0415a405bf768092ff5
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2024-01-19 20:28:02 -0500

    new physics-based movement

--- Scripts/Collectable2D.gd
+++ Scripts/Collectable2D.gd
@@ -14,7 +14,9 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("Squitos"):
-		if not collectors.get(body.id,false):
-			collectors[body.id] = true
-			print(str(body.id, "  collected ",name))
+		if not collectors.get(body.name,false):
+			collectors[body.name] = true
+			print(str(body.name, " collected ",name))
 			# TODO: give squito energy
+		else:
+			print(str(body.name," has already collected this ",name))
--- Scripts/SquitoCopter.gd
+++ Scripts/SquitoCopter.gd
@@ -2,42 +2,39 @@ extends RigidBody2D
 
 ## number of times per second an action is taken
 @export var frequency : float = 1
-## hove far this copter moves
-@export var step_size : float
-## Speed (in pixels per second) at which copter takes steps
-@export var speed : float
+## force size for movement
+@export var move_force : float
 ## number of directions this can face
 @export_range(4,256) var directions : int = 4
 ## actions this instance will loop over
 @export var genome : Array[int]
-## Id for interraction for this instance
-@export var id : String
 var step : int = 0
 var actions : Array[Callable] = [
+	wait,
+	move_forward,
 	turn_left,
 	turn_right,
-	move_forward,
-	wait
 ]
 var _action_counter : float = 0
 # 0=up,1=left,2=down,3=right
 var _facing : int = 0
-var _moving : bool = false
-var _end_move : Vector2
-
-# TODO: energy system
+# the visual, this is rotated to indicate where the next force will push
+@onready var sprite = get_node("Sprite")
+@onready var moveForceVec = Vector2.UP*move_force
+var move:Vector2 = Vector2()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if _moving:
-		global_position = global_position.move_toward(_end_move,speed)
-		if global_position == _end_move:
-			_moving = false
-	else:
-		_action_counter += delta*frequency
-		if _action_counter >= 1:
-			do_action()
-			_action_counter = 0
+	_action_counter += delta*frequency
+	if _action_counter >= 1:
+		do_action()
+		_action_counter = 0
+
+func _integrate_forces(state):
+	state.apply_central_force(move)
+	if move.length_squared() != 0:
+		print(move)
+		move = Vector2()
 
 func do_action():
 	var action = genome[step]
@@ -46,18 +43,14 @@ func do_action():
 
 func turn_left():
 	_facing = (_facing+1)%directions
-	rotation = _facing_to_rotation()
+	sprite.rotation = _facing_to_rotation()
 
 func turn_right():
 	_facing = (_facing+3)%4
-	rotation = _facing_to_rotation()
+	sprite.rotation = _facing_to_rotation()
 
 func move_forward():
-	print("Moving forward")
-	_moving = true
-	var move_dir = Vector2.UP * step_size
-	move_dir = move_dir.rotated(_facing_to_rotation())
-	_end_move = global_position+move_dir
+	move = moveForceVec.rotated(_facing_to_rotation())
 
 func wait():
 	print("Waiting")
